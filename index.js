1736f89c24ae47ee42686d708cbac542ab2ae799
TheDevYellowy <yellowycorn6963@gmail.com>
2022-04-01 19:19:04 -0500

    I want to rip my hair out after figuring this out

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
 const os = require('os');
+const { sep } = require('path');
+const axios = require('axios').default;
+const sleep = sec => new Promise(r => setTimeout(r, (sec * 1000)));
 /**
    * 
-   * @param {*} client Client
    * @param {string} username Username of the user you want to log into
    * @param {string} descriminator descriminator of the user if you have multiple accounts with the same name
    * @private
    */
-function getToken(client, username, descriminator) {
+async function getToken(username, descriminator) {
     var token = /[A-Za-z\d]{24}\.[\w]{6}\.[\w]{27}/;
     var mfaToken = /mfa\.[\w-]{84}/;
     var WinLocation = `C:${sep}Users${sep}${os.userInfo().username}${sep}AppData${sep}Roaming`;
-    const headers = client.options.http.headers;
 
     var x = [];
     var y = [];
@@ -20,20 +21,26 @@ function getToken(client, username, descriminator) {
     let found = false;
 
     const getSortedFiles = async (dirs) => {
+        var temp = [];
         dirs.forEach(async dir => {
             dir += `${sep}Local Storage${sep}leveldb`;
             var exists = fs.existsSync(dir);
-            console.log(exists);
 
             if (!exists) return null;
 
             let files = await fs.promises.readdir(dir);
             files.forEach(f => {
-                if (f.endsWith('.ldb')) x.push(`${dir}${sep}${f}`)
-                else if (f.endsWith('.log')) x.push(`${dir}${sep}${f}`);
+                if (f.endsWith('.ldb')) {
+                    temp.push(`${dir}${sep}${f}`)
+                }
+                else if (f.endsWith('.log')) {
+                    temp.push(`${dir}${sep}${f}`)
+                }
                 else return;
             });
         });
+
+        return temp;
     }
 
     if (os.type().toLowerCase().includes('windows')) {
@@ -43,80 +50,67 @@ function getToken(client, username, descriminator) {
             `${WinLocation}${sep}discordcanary`,
         ]
 
-        Promise.resolve().then(() => {
-            getSortedFiles(paths);
-        }).catch(console.error);
+        x = await getSortedFiles(paths);
 
         setTimeout(async () => {
-            x.forEach(f => {
-                console.log(f);
+            x.forEach(async f => {
                 fs.readFile(f, function (err, data) {
                     if (err) throw err;
                     data = data.toString();
 
                     if (token.test(data)) {
-                        console.log('found one')
                         let z = token.exec(data);
 
-                        z.forEach(t => {
-                            if (y.indexOf(t) === -1) y.push(t);
-                        });
+                        let t = z[0];
+                        if(token.test(t)) if (y.indexOf(t) === -1) y.push(t);
                     } else if (mfaToken.test(data)) {
-                        console.log('found one')
                         let z = mfaToken.exec(data);
 
-                        z.forEach(t => {
-                            if (y.indexOf(t) === -1) y.push(t);
-                        });
+                        let t = z[0];
+                        if (mfaToken.test(t)) if (y.indexOf(t) === -1) y.push(t);
                     }
                 });
             });
-        }, 500);
 
-        y.forEach(t => {
-            if (!found) {
-                axios.get('https://discord.com/api/v9/users/@me', {
-                    headers: {
-                        "Accept": "*/*",
-                        "Accept-Encoding": "gzip, deflate, br",
-                        "Accept-Language": 'en-US,en;q=0.9',
-                        "Cache-Control": "no-cache",
-                        "Pragma": "no-cache",
-                        "authorization": t,
-                        "Referer": "https://discord.com/channels/@me",
-                        "Sec-Ch-Ua": '" Not A;Brand";v="99" "',
-                        "Sec-Ch-Ua-Mobile": '?0',
-                        "Sec-Ch-Ua-Platform": '"iOS"',
-                        "Sec-Fetch-Dest": "empty",
-                        "Sec-Fetch-Mode": "cors",
-                        "Sec-Fetch-Site": "same-origin",
-                        "X-Debug-Options": "bugReporterEnabled",
-                        "X-Discord-Locale": 'en-US',
-                        "Origin": "https://discord.com"
-                    }
-                }).then(res => {
-                    const d = res.data;
-                    if (d.username == username) {
-                        if (descriminator !== null) {
-                            if (d.descriminator == descriminator) {
-                                found = true
+            setTimeout(() => {
+                y.forEach(async t => {
+                    if (found === false) {
+                        await axios.get('https://discord.com/api/v7/users/@me', {
+                            headers: {
+                                "Content-Type": "application/json",
+                                "authorization": t,
+                            }
+                        }).then(async (res) => {
+                            const d = res.data;
+                            if (d.username == username) {
+                                if (descriminator !== null) {
+                                    if (d.descriminator == descriminator) {
+                                        found = true
+                                        userToken = t;
+                                        return;
+                                    } else return null;
+                                }
+                                found = true;
                                 userToken = t;
                             }
-                        }
-                        found = true;
-                        userToken = t;
+                        }).catch(e => {
+                            if(e.toString().includes('401')) return;
+                            else console.error(e);
+                        });
                     }
-                }).catch(e => {
-                    throw e;
                 });
-            }
-        });
+            }, 2000)
 
-        if (!found) return null;
-        else return userToken;
+            
+        }, 500);
     } else {
         throw new Error(`This is not yet developed on this os`);
     }
+
+    await sleep(3);
+
+    if (!found) return 'No Token';
+    else return userToken;
 };
 
 module.exports = getToken;
